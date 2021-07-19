43869e4412ff2ed9692f1ae3b78480d30aec962d
CHETcica <61766789+CHETcica@users.noreply.github.com>
2021-07-19 15:51:57 +0700

    Initial commit

---
+++ src/com/swa/oop5/TestPerson.java
@@ -0,0 +1,11 @@
+package com.swa.oop5;
+
+public class TestPerson {
+
+    public static void main(String[] args) {
+        Person person = new Person();
+        long salary = person.getSalary();
+    }
+
+}
+
